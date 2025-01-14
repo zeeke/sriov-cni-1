d74579b1d7564ba7c11a0b9e1279da33deb010e4
Andrea Panattoni <apanatto@redhat.com>
2025-01-14 18:44:25 +0100

    mock

--- cmd/mocked-sriov/main.go
+++ cmd/mocked-sriov/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"runtime"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
@@ -9,6 +10,13 @@ import (
 	"github.com/k8snetworkplumbingwg/sriov-cni/pkg/config"
 	"github.com/k8snetworkplumbingwg/sriov-cni/pkg/utils"
 )
+
+func init() {
+	// this ensures that main runs only on main thread (thread group leader).
+	// since namespace ops (unshare, setns) are done for a single thread, we
+	// must ensure that the goroutine does not jump from OS thread to thread
+	runtime.LockOSThread()
+}
 
 func main() {
 
